fc7d5f23bd5e15cb89e623f56b3ac497d69156ae
MarqusJonsson <markusjonsson1997@gmail.com>
2022-11-09 14:09:05 +0100

    Model test with multiple GPUs

--- src/test_model.py
+++ src/test_model.py
@@ -1,3 +1,6 @@
+import os
+GPUS =  "5,6,7"
+os.environ["CUDA_VISIBLE_DEVICES"] = "5,6,7"
 import torch
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
@@ -9,11 +12,14 @@ from utils import (
 	calc_dice,
 	calc_jaccard,
 	check_performance,
+	getDatetime,
 	load_checkpoint,
 	save_checkpoint,
 	get_test_loader,
+	save_dict_as_csv,
 	save_predictions_as_imgs,
 )
+import time;
 
 # Hyperparameters etc.
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -22,14 +28,32 @@ NUM_WORKERS = 2
 PIN_MEMORY = True
 LOAD_MODEL = False
 TEST_IMG_DIR = "../data/patches/ps_1024_po_0.5_mt_0.8/test/tissue"
-TEST_MASK_DIR = "../data/patches/ps_1024_po_0.5_mt_0.8/test/whole"
+TEST_MASK_DIR = "../data/patches/ps_1024_po_0.5_mt_0.8/test/viable"
+ENCODER = "resnet50"
+BCE_WEIGHT = 0.1
+TEST_INFO_FILENAME = "test_results.csv"
+MODEL_LOAD_PATH = "saved_models/unet_resnet50/imagenet_weights/no_preproc/viable/20221028/e_19_l_0.1133_d_20221028T110244Z.pt"
 
 def main():
 	test_transforms = A.Compose([
 		ToTensorV2(),
 	])
 
-	model = smp.Unet(encoder_name="resnet50", in_channels=3, classes=1).to(device=DEVICE)
+	model = smp.Unet(encoder_name=ENCODER, in_channels=3, classes=1).to(device=DEVICE)
+	model = torch.nn.DataParallel(model)
+	model = model.to(device=DEVICE)
+
+	def calc_loss(pred, target, bce_weight=BCE_WEIGHT):
+		bce = F.binary_cross_entropy_with_logits(pred, target)
+
+		pred = torch.sigmoid(pred)
+		dice = calc_dice(pred, target)
+		# jaccard = calc_jaccard(pred, target)
+		loss = bce * bce_weight + (1.0 - dice) * (1.0 - bce_weight)
+
+		return loss
+
+	loss_fn = calc_loss
 
 	test_loader = get_test_loader(
 		TEST_IMG_DIR,
@@ -40,12 +64,27 @@ def main():
 		PIN_MEMORY,
 	)
 
-	load_checkpoint(torch.load("saved_models/unet_resnet50/imagenet_weights/no_preproc/0709_e_4_j_0.8281_d_0.9033_a_0.9086.pth.tar"), model)
-	check_performance(test_loader, model, device=DEVICE)
+	load_checkpoint(torch.load(MODEL_LOAD_PATH), model)
+	# check test set perfromance
+	print("Checking test set performance...")
+	test_set_performance_start_time = time.time()
+	test_set_performance = check_performance(test_loader, model, loss_fn, result_prefix="test_", device=DEVICE)
+	check_test_set_perfromance_time = time.time() - test_set_performance_start_time
+	currentDatetime = getDatetime()
+
+	test_info = {
+		**test_set_performance,
+		"check_test_set_perfromance_time": check_test_set_perfromance_time,
+		"datetime": currentDatetime,
+		"model_path": MODEL_LOAD_PATH,
+	}
+
+	# save test info to csv file
+	save_dict_as_csv(TEST_INFO_FILENAME, test_info)
 
 	# print some examples to a folder
-	save_predictions_as_imgs(test_loader, model, folder="pred_samples/", device=DEVICE)
+	# save_predictions_as_imgs(test_loader, model, folder="pred_samples/", device=DEVICE)
 
 if __name__ == "__main__":
-	torch.cuda.set_device(7)
+	# torch.cuda.set_device(7)
 	main()
